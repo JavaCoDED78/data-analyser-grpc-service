1134cb2a4c2cd8054296aa66932a2e44a0bb4fb1
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2024-06-09 19:04:37 +0300

    init commit

---
+++ src/main/java/com/javaded/DataAnalyserGrpcServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javaded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAnalyserGrpcServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataAnalyserGrpcServiceApplication.class, args);
+    }
+
+}
